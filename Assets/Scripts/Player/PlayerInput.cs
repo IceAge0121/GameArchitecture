8f8b1aec70ee9b5a3fa0c17971f388f3fb3f708b
Isiaq Gbadamosi <badmusishaq@gmail.com>
2023-06-16 04:47:52 +0100

    Class 8

--- Assets/Scripts/Player/PlayerInput.cs
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -19,6 +19,8 @@ public class PlayerInput : MonoBehaviour
     public bool secondaryShootPressed { get; private set; }
     public bool weapon1Pressed { get; private set; }
     public bool weapon2Pressed { get; private set; }
+    public bool commandPressed { get; private set; }
+
 
     private bool clear;
 
@@ -65,6 +67,8 @@ public class PlayerInput : MonoBehaviour
 
         weapon1Pressed = weapon1Pressed || Input.GetKeyDown(KeyCode.Alpha1);
         weapon2Pressed = weapon2Pressed || Input.GetKeyDown(KeyCode.Alpha2);
+
+        commandPressed = commandPressed || Input.GetKeyDown(KeyCode.C);
     }
 
     private void FixedUpdate()
@@ -93,5 +97,7 @@ public class PlayerInput : MonoBehaviour
 
         weapon1Pressed = false;
         weapon2Pressed = false;
+
+        commandPressed = false;
     }
 }
